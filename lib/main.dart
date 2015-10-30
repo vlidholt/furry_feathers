9311560c0244aeaf54ce6600487abbd68dd08934
Viktor Lidholt <viktorl@google.com>
2015-10-30 10:10:01 -0700

    Adds scrollview and asset loading

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_sprites/flutter_sprites.dart';
 
 AssetBundle _initBundle() {
@@ -13,16 +14,21 @@ AssetBundle _initBundle() {
 final AssetBundle _bundle = _initBundle();
 
 ImageMap _images;
+SpriteSheet _sprites;
 
 main() async {
   _images = new ImageMap(_bundle);
 
   List loads = [];
   loads.add(_images.load(<String>[
-    'assets/background.png'
+    'assets/background.png',
+    'assets/sprites.png'
   ]));
 
   await Future.wait(loads);
+
+  String json = await _bundle.loadString('assets/sprites.json');
+  _sprites = new SpriteSheet(_images['assets/sprites.png'], json);
 
   runApp(
     new MaterialApp(
@@ -36,18 +42,32 @@ main() async {
 
 class FurryFeathers extends StatelessComponent {
   Widget build(BuildContext context)  {
-    return new SpriteWidget(new FurryFeathersNode());
+    return new ScrollableViewport(
+      scrollDirection: ScrollDirection.horizontal,
+      child: new Container(
+        width: 4096.0,
+        height: 1280.0,
+        child: new SpriteWidget(new FurryFeathersNode(), SpriteBoxTransformMode.fixedHeight)
+      )
+    );
   }
 }
 
 class FurryFeathersNode extends NodeWithSize {
-  FurryFeathersNode() : super(new Size(1024.0, 1024.0)) {
-    Label lbl = new Label("Hello");
-    lbl.position = new Point(512.0, 512.0);
-    addChild(lbl);
-
+  FurryFeathersNode() : super(new Size(4096.0, 1280.0)) {
     Sprite bg = new Sprite.fromImage(_images['assets/background.png']);
-    bg.position = new Point(512.0, 512.0);
+    bg.pivot = Point.origin;
+    bg.position = Point.origin;
     addChild(bg);
+
+    userInteractionEnabled = true;
+  }
+
+  bool handleEvent(SpriteBoxEvent event) {
+    if (event.type == "pointerdown") {
+      print("Pointer down");
+      return true;
+    }
+    return false;
   }
 }
